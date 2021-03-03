5543c0221e3d3470f0551cdb7bdacc186cc18739
Carlos Rosado <krlos_1594@yahoo.com>
2021-03-03 11:07:42 -0600

    first commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,93 @@
+const path = require('path') 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebapckPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name][contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+    },
+    mode: 'development',
+    watch: true,
+    resolve: {
+        extensions: ['js'],
+        alias:{
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+          }
+    },
+    devServer: {
+        port: 9000,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: 'application/font-woff',
+                        name: '[name].[contenthash].[ext]',
+                        outputPath: './assets/fonts/',
+                        publicPath: '../fonts/',
+                        esModule: false
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebapckPlugin({
+            template: path.resolve(__dirname, './public/index.html'),
+            filename: 'public/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'assets/images'),
+                    to: 'assets/images'
+                }
+            ]
+        })
+    ],
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [
+    //         new CssMinimizerPlugin(),
+    //         new TerserPlugin()
+    //     ]
+    // }
+
+}
